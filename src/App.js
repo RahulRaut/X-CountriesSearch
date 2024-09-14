95a14e29da3a4cc42e84cb58b2580a122dac7238
RahulRaut <35568456+RahulRaut@users.noreply.github.com>
2024-09-14 11:51:04 +0530

    app commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
